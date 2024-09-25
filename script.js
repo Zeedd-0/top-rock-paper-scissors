4fc7e2869ba291b9d984076ddc4df65aecf7fe75
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-25 10:50:31 -0300

    Create getHumanChoice() and verifyInput()
    Test input by prompt with while and short-break
    and return it to choice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// get computer choice by random and return it
 function getComputerChoice() {
     let choice = Math.floor((Math.random() + 0.1)* 10);
     if (choice < 10/3) {
@@ -9,4 +10,20 @@ function getComputerChoice() {
     }
     console.log(choice);
     return choice;
+}
+
+
+// get human choice by tested and valid input
+function getHumanChoice() {
+    let choice = verifyInput();
+    return choice;
+}
+// teste human input and return it if valid
+function verifyInput() {
+    let keepGoing = 1;
+    while (keepGoing) {
+        input = prompt('What is your play?\n- rock\n- paper\n- scissors').toLowerCase().trim();
+        keepGoing = !(input === 'rock' || input === 'paper' || input === 'scissors' || 0);
+    }
+    return input;
 }
